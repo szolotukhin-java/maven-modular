f31546c007fb9f852df4d3b59191d800686b28ff
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2023-08-29 20:32:44 +0300

    simple project

---
+++ module1/src/main/java/ua/in/sz/mvn/modular/first/App.java
@@ -0,0 +1,9 @@
+package ua.in.sz.mvn.modular.first;
+
+public class App {
+    public static final String GREETING = "Hello World!";
+
+    public static void main(String[] args) {
+        System.out.println(GREETING);
+    }
+}
---
+++ module1/src/test/java/ua/in/sz/mvn/modular/first/AppTest.java
@@ -0,0 +1,19 @@
+package ua.in.sz.mvn.modular.first;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class AppTest extends TestCase {
+    public AppTest(String testName) {
+        super(testName);
+    }
+
+    public static Test suite() {
+        return new TestSuite(AppTest.class);
+    }
+
+    public void testApp() {
+        assertTrue(true);
+    }
+}
